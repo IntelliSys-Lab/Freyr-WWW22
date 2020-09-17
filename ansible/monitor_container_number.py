11d67f1b0f18db47231ae4b92a455ab937946d23
hanfeiyu <hanfeiyu@uw.edu>
2020-09-17 06:50:49 +0000

    Fix container number monitor script

--- ansible/monitor_container_number.py
+++ ansible/monitor_container_number.py
@@ -6,20 +6,24 @@ import redis
 
 
 # Connect to redis pool
-host = "192.168.196.213"
-port = 6379
-password = "openwhisk"
+redis_host = "192.168.196.213"
+redis_port = 6379
+redis_password = "openwhisk"
 
-pool = redis.ConnectionPool(host=host, port=port, password=password, decode_responses=True)
+pool = redis.ConnectionPool(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
 
+# Get invoker name
 invoker_hostname = socket.gethostname()
 invoker_ip = socket.gethostbyname(invoker_hostname)
+invoker = os.popen(
+    'cat environments/distributed/hosts | grep "invoker" | grep "{}" | awk {}'.format(invoker_ip, "{'print $1'}")
+).read().replace('\n', '')
 
 # Monitor alive container number
 while True:
     time.sleep(0.5)
     n_container = os.popen('docker ps | grep "guest" | wc -l').read()
-    r.hset(invoker_ip, "n_container", n_container)
+    r.hset(invoker, "n_container", n_container)
     
 
